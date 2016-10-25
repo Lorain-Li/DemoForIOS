9bcfd8161fe3b208cae54fd7e32d6e62bf63eaaf
lorainlynies <lorainlynies@outlook.com>
2016-10-25 07:24:01 -0700

    make view move.

--- 10TimerMoveViews/10TimerMoveViews/ViewController.m
+++ 10TimerMoveViews/10TimerMoveViews/ViewController.m
@@ -19,12 +19,16 @@
     [super viewDidLoad];
     UIButton* _myButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     CGRect _winRect = [[UIScreen mainScreen] bounds];
-    _myButton.frame = CGRectMake(50, 200, _winRect.size.width - 100, 50);
+    _myButton.frame = CGRectMake(50, 300, _winRect.size.width - 100, 50);
     _myButton.backgroundColor = [UIColor blueColor];
     [_myButton setTitle:@"Start/Stop" forState:UIControlStateNormal];
     [_myButton setTitle:@"loose me" forState:UIControlStateHighlighted];
     [_myButton addTarget:self action:@selector(pressDown) forControlEvents:UIControlEventTouchDown];
+    UIView* _myView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    _myView.backgroundColor = [UIColor redColor];
+    _myView.tag = 101;
     [self.view addSubview:_myButton];
+    [self.view addSubview:_myView];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -36,9 +40,9 @@
 
 - (void) pressDown
 {
-    if(_myTimer == nil)
+    if(_myTimer == nil | _myTimer.valid == FALSE)
     {
-     _myTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(timerMove) userInfo:nil repeats:TRUE];
+     _myTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(timerMove) userInfo:nil repeats:TRUE];
     }
     else{
         [_myTimer invalidate];
@@ -48,5 +52,7 @@
 - (void) timerMove
 {
     NSLog(@"1\r\n");
+    UIView* _theView = [self.view viewWithTag:101];
+    _theView.frame = CGRectMake(_theView.frame.origin.x+1, _theView.frame.origin.y+1, 100, 100);
 }
 @end
